629deb05b53d0b3691fea4c1f4f1b26276e54b65
Very Shafrudin <very.shafrudin@gmail.com>
2022-07-26 02:53:56 +0700

    - connection local

--- helpers/mysql-conn.js
+++ helpers/mysql-conn.js
@@ -6,28 +6,11 @@ const fs = require('fs')
 exports.knex = require('knex')({
     client: 'mysql',
     connection: {
-      user: 'bimtek',
-      password: 'AVNS_-qJiWVGNZWanOQFCIXV',
-      database: 'bimtek',
-      host: 'private-db-mysql-blr1-27443-do-user-2156672-0.b.db.ondigitalocean.com',
-      port: 25060,
-      dialect: 'mysql',
-      logging: true,
-      force: true,
-      timezone: 'Asia/Jakarta',
-      pool: {
-          max: 100,
-          min: 0,
-          idle: 200000,
-          acquire: 1000000,
-      },
-      ssl: true,
-      dialectOptions: {
-          ssl: {
-              ssl: true,
-              cert: fs.readFileSync('../ca-certificate.crt')
-          }
-      }
+      host : process.env.MYSQL_HOST,
+      user : process.env.MYSQL_USERNAME,
+      password : process.env.MYSQL_PASSWORD,
+      database : process.env.MYSQL_DBNAME,
+      timezone : 'Asia/Jakarta',
     },
     pool: { min: 0, max: 7  }
 });
