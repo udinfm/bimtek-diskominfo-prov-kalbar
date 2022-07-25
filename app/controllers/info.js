15229e75659599d430a5524a079ee82649bb364a
Very Shafrudin <very.shafrudin@gmail.com>
2022-07-26 01:09:06 +0700

    - concurrent

--- app/controllers/info.js
+++ app/controllers/info.js
@@ -23,3 +23,72 @@ exports.jumlahData = (req,res)=>{
         })
     })
 }
+
+//daftar data sequential
+exports.sequential = async (req,res)=>{
+    try{
+        let page = req.params.page || 1
+        let limit = req.params.limit || 10
+
+        //parse params
+        page = parseInt(page)
+        limit = parseInt(limit)
+
+
+        const jumlahData = await getter.jumlahData()
+        const jumlahHalaman = await getter.jumlahHalaman(limit)
+        const daftarData = await getter.daftarData(page,limit)
+
+        res.send({
+            hasil: "success",
+            data: {
+                jumlahdata: jumlahData,
+                jumlahhalaman: jumlahHalaman,
+                data: daftarData
+            }
+        })
+
+    }catch(err){
+        console.log('terjadi error',err)
+        res.send({
+            hasil: "error",
+            pesan: err
+        })
+    }
+}
+
+//daftar data concurrent
+exports.concurrent = async (req,res)=>{
+    try{
+        let page = req.params.page || 1
+        let limit = req.params.limit || 10
+
+        //parse params
+        page = parseInt(page)
+        limit = parseInt(limit)
+
+        const dataModel = await Promise.all(
+            [
+                getter.jumlahData(),
+                getter.jumlahHalaman(limit),
+                getter.daftarData(page,limit)
+            ]
+        )
+
+        res.send({
+            hasil: "success",
+            data: {
+                jumlahdata: dataModel[0],
+                jumlahhalaman: dataModel[1],
+                data: dataModel[2]
+            }
+        })
+
+    }catch(err){
+        console.log('terjadi error',err)
+        res.send({
+            hasil: "error",
+            pesan: err
+        })
+    }
+}
--- routes/all.js
+++ routes/all.js
@@ -7,4 +7,10 @@ const Info = require("@app/controllers/info")
 
 module.exports = app=>{
     app.get("/JumlahData/",Info.jumlahData)//jumlah data
+
+    //sequential
+    app.get('/Sequential/:page/:limit',Info.sequential)
+
+    //concurrent
+    app.get('/Concurrent/:page/:limit',Info.concurrent)
 }
